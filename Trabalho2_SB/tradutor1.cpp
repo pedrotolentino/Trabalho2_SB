1ca3f1f8adaeebf2d2e03e60ed75822c75b79635
Pedro Henrique Souza Tolentino <pedro_toletino12@hotmail.com>
2015-11-30 23:16:17 -0200

    Inserção das funções de leitura e escrita de Strings e leitura de inteiros

--- Trabalho2_SB/tradutor1.cpp
+++ Trabalho2_SB/tradutor1.cpp
@@ -131,6 +131,15 @@ int main ()
             else
             {
                 out << "section .data\n";
+                out << "cont 	db 0"<<endl;
+                out << "char	db 0"<<endl;
+                out << "tam_char equ $-char"<<endl;
+				out << "num 	dd 0"<<endl;
+				out << "tamNum	equ	4"<<endl;
+				out << "string		times 100 db 0"<<endl;
+				out << "tamString   equ 100"<<endl;
+				out << "msg_erro	db 'Tentativa de acesso a memoria nao reservada... Programa será encerrado', 0x0d, 0x0a"<<endl;
+				out << "tam_m_erro	equ $-msg_erro"<<endl;
             }
         }
         /*caso nao for section*/
@@ -156,64 +165,84 @@ int main ()
             {
                 switch(inst)
                 {
-                    case 0: /*Nao eh uma instrucao*/
-                    break;
-                    case 1: /*ADD*/
-                        out << "add eax, " << tokens[j][i+1] << "\n";
-                    break;
-                    case 2: /*SUB*/
-                        out << "sub eax, " << tokens[j][i+1] << "\n"; 
-                    break;
-                    case 3: /*MUL*/
-                        out << "mov ebx, " << tokens[j][i+1] << "\n";
-                        out << "mul ebx" << "\n";
-                    break;
-                    case 4: /*DIV*/
-                        out << "mov ebx, " << tokens[j][i+1] << "\n";
-                        out << "div ebx" << "\n";
-                    break;
-                    case 5: /*JMP*/
-                        out <<  "jmp " << tokens[j][i+1] << "\n";
-                    break;
-                    case 6: /*JMPN*/
-                        out << "js eax, " << tokens[j][i+1] << "\n";
-                    break;
-                    case 7: /*JMPP*/
-                        out << "jns eax, " << tokens[j][i+1] << "\n";
-                    break;
-                    case 8: /*JMPZ*/
-                        out << "cmp eax, 0\n";
-                        out << "je " << tokens[j][i+1] << "\n";
-                    break;
-                    case 9: /*COPY*/
-                        out << "mov " << tokens[j][i+1] << ", " << tokens[j][i+2] << "\n";
-                    break;
-                    case 10: /*LOAD*/
-                        out << "mov eax, "<< tokens[j][i+1] << "\n";
-                    break;
-                    case 11: /*STORE*/
-                        out << "mov [" << tokens[j][i+1] << "], eax" << "\n";
-                    break;
-                    case 12: /*INPUT*/
-//                        out << "mov eax, 3\n";
-//                        out << "mov ebx, 0\n";
-//                        out << "mov ecx, " << tokens[j][i+1] << "\n";
-//                        out << "mov edx, 4\n";
-//                        out << "int 80h\n";
-                        out << "call lerInteiro" << "\n";
-                    break;
-                    case 13: /*OUTPUT*/
-                        out << "mov eax, 4\n";
-                        out << "mov ebx, 1\n";
-                        out << "mov ecx, " << tokens[j][i+1] << "\n";
-                        out << "mov edx, 4\n";
-                        out << "int 80h\n";
-                    break;
-                    case 14: /*STOP*/
-                        out << "mov eax, 1" << "\n";
-                        out << "mov ebx, 0" << "\n";
-                        out << "int 80h" << "\n";
-                    break;
+                	case 1: /*ADD*/
+						out << "add eax, " << "\n";
+					break;
+					case 2: /*SUB*/
+						out << "sub eax, " << "\n";
+					break;
+					case 3: /*MUL*/
+						out << "mul eax, " << "\n";
+					break;
+					case 4: /*DIV*/
+						out << "div eax, " << "\n";
+					break;
+					case 5: /*JMP*/
+						out <<  "jmp " << tokens[j][i+1] << "\n";
+					break;
+					case 6: /*JMPN*/
+						out << "js eax, " << tokens[j][i+1] << "\n";
+					break;
+					case 7: /*JMPP*/
+						out << "jns eax, " << tokens[j][i+1] << "\n";
+					break;
+					case 8: /*JMPZ*/
+						out << "cmp eax, 0\n";
+						out << "je " << tokens[j][i+1] << "\n";
+					break;
+					case 9: /*COPY*/
+						out << "mov " << tokens[j][i+1] << ", " << tokens[j][i+2] << "\n";
+					break;
+					case 10: /*LOAD*/
+						out << "mov eax, "<< tokens[j][i+1] << "\n";
+					break;
+					case 11: /*STORE*/
+						out << "mov " << tokens[j][i+1] << ", eax\n";
+					break;
+					case 12: /*INPUT*/
+	//                        out << "mov eax, 4\n";
+	//                        out << "mov ebx, 1\n";
+	//                        out << "mov ecx, " << tokens[j][i+1] << "\n";
+	//                        out << "mov edx, 3\n";
+	//                        out << "int 80h\n";
+						out<<"call lerInteiro"<<endl;
+						out<<"mov DWORD ["<<tokens[j][i+1]<<"], [eax]"<<endl;
+					break;
+					case 13: /*OUTPUT*/
+	//                        out << "mov eax, 3\n";
+	//                        out << "mov ebx, 0\n";
+	//                        out << "mov ecx, " << tokens[j][i+1] << "\n";
+	//                        out << "mov edx, 3\n";
+	//                        out << "int 80h\n";
+						out<<"mov eax, ["<<tokens[j][i+1]<<"]"<<endl;
+						out<<"mov esi, 0"<<endl;
+						out<<"call escreverInteiro"<<endl;
+					break;
+					case 14: /*STOP*/
+						out << "mov eax, 1\n";
+						out << "mov ebx, 0\n";
+						out << "int 80h\n";
+					break;
+					case 15: /*C_INPUT*/
+						out<<"call lerChar"<<endl;
+						out<<"mov ["<<tokens[j][i+1]<<"], [eax]"<<endl;
+						break;
+					case 16: /*C_OUTPUT*/
+						out<<"mov eax, ["<<tokens[j][i+1]<<"]"<<endl;
+						out<<"call escreverChar"<<endl;
+						break;
+					case 17: /*S_INPUT*/
+						out<<"call lerString"<<endl;
+						out<<"mov ["<<tokens[j][i+1]<<"], eax"<<endl;
+						break;
+					case 18: /*S_OUTPUT*/
+						out<<"mov eax, ["<<tokens[j][i+1]<<"]"<<endl;
+						out<<"mov esi, 0"<<endl;
+						out<<"call escreverChar"<<endl;
+						break;
+					default:
+						cout<<"Opcode inválido"<<endl;
+						break;
                 }
             }
             else /*caso nao for instrucao*/
@@ -253,7 +282,10 @@ void escreverInput(ofstream& arq){
 }
 
 void escreverOutput(ofstream& arq){
-    arq<<""<<endl;
+    arq<<"escreverInteiro:\tmov edx, 0\n\t\tmov ebx, 0XA\n\t\tdiv ebx\n\t\tadd edx, 0X30\n\t\tmov [num+esi], edx\n\t\tinc esi\n\t\t"
+    		"cmp DWORD eax, 0\n\t\tje f_escreverInteiro\n\t\tjmp escreverInteiro\n\t\tf_escreverInteiro:\tmov eax, 4\n\t\t"
+    		"mov ebx, 1\n\t\tlea ecx, [num+esi]\n\t\tmov edx, 1\n\t\tint 0X80\n\t\tdec esi\n\t\tcmp DWORD esi, 0\n\t\t"
+    		"jge f_escreverInteiro\n\t\tret"<<endl;
 }
 
 void escreverC_input(ofstream& arq){
@@ -266,9 +298,16 @@ void escreverC_output(ofstream& arq){
 }
 
 void escreverS_input(ofstream& arq){
-    arq<<""<<endl;
+    arq<<"lerString:\tmov BYTE [cont], 0x0\n\t\tmov esi, 0\n\t\tjmp inicializaString\n\t\tlacoLeitura:\tmov eax, 3\n\t\tmov ebx, 0\n\t\t"
+    		"mov ecx, string\n\t\tmov edx, tamString\n\t\tint 0x80\n\t\tmov esi, 99\n\t\tcmp BYTE [string+esi], 0x0\n\t\tje fimLeitura\n\t\t"
+    		"cmp BYTE [string+esi], 0xA\n\t\tje fimLeitura\n\t\tjmp lancarMsgErro\n\t\tfimLeitura:	ret\n\t\t"
+    		"inicializaString: cmp BYTE [cont], 100\n\t\tje lacoLeitura\n\t\tmov BYTE [string+esi], 0\n\t\tinc BYTE [cont]\n\t\t"
+    		"inc esi\n\t\tjmp\n\t\tinicializaStringlancarMsgErro:\tmov eax, 4\n\t\tmov ebx, 1\n\t\tmov ecx, msg_erro\n\t\t"
+    		"mov edx, tam_m_erro\n\t\tint 0x80\n\t\tmov eax, 1\n\t\tmov ebx, 0\n\t\tint 0x80"<<endl;
 }
 
 void escreverS_output(ofstream& arq){
-    arq<<""<<endl;
+    arq<<"escreverString:\tmov BYTE bl, [string+esi]\n\t\tcmp bl , 0xA\n\t\tje fimEscrita\n\t\tmov eax, 4\n\t\tmov ebx, 1\n\t\t"
+    		"lea ecx, [string+esi]\n\t\tmov edx, 1\n\t\tint 0x80\n\t\tinc esi\n\t\tjmp escreverString\n\t\tfimEscrita:\tmov eax, 4\n\t\t"
+    		"mov ebx, 1\n\t\tlea ecx, [string+esi]\n\t\tmov edx, 1\n\t\tint 0x80\n\t\tret"<<endl;
 }
